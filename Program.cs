26040f6730e6a174581b348dea6208eac6224b31
Saryuna <s.syrenova.07@mail.ru>
2026-02-12 00:37:52 +0700

    Добавлен ExceptionMiddleware для глобальной обработки ошибок

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 using CollegeSchedule.Data;
-using Microsoft.EntityFrameworkCore;
-using DotNetEnv;
+using CollegeSchedule.Middlewares;
 using CollegeSchedule.Services;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
