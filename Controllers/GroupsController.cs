984da3079f417e0270f90bc75131c0a59eee5292
Saryuna <s.syrenova.07@mail.ru>
2026-02-18 16:18:02 +0700

    Создан контроллергрупп с эндпоинтом гет api/groups

---
+++ Controllers/GroupsController.cs
@@ -0,0 +1,25 @@
+﻿using CollegeSchedule.DTO;
+using CollegeSchedule.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeSchedule.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GroupsController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+
+        public GroupsController(IScheduleService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<GroupDto>>> GetGroups()
+        {
+            var groups = await _service.GetAllGroups();
+            return Ok(groups);
+        }
+    }
+}
